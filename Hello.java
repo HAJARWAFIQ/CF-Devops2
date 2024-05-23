2968d6e2d19f20a6bec14bb4047e8cfbd1e666c1
Hajar wafiq <wafiqhajaar@gmail.com>
2024-05-23 15:20:07 +0100

    first commit on branch dev

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         // Affiche les messages à la console
-        System.out.println("Bonjour Master devops");
+        System.out.println("Bonjour Master devops 1");
         System.out.println("Bon courage dans votre examen");
     }
 }
