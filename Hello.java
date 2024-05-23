ceb9b8b5f13df123bd79d64af67eeac2f22e1375
Hajar wafiq <wafiqhajaar@gmail.com>
2024-05-23 18:39:07 +0100

    add more & more content

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         // Affiche les messages à la console
         System.out.println("Bonjour Master devops Bonjour ");
-        System.out.println("Bon courage dans votre examen");
+        System.out.println("Bon courage dans votre examen une autre fois ");
     }
 }
