09b09bbf0b45bccd9c7d1bea65b64d0fbcc51354
Hajar wafiq <wafiqhajaar@gmail.com>
2024-05-23 15:21:37 +0100

    add more content on branch dev

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         // Affiche les messages à la console
-        System.out.println("Bonjour Master devops 1");
+        System.out.println("Bonjour Master devops Bonjour ");
         System.out.println("Bon courage dans votre examen");
     }
 }
