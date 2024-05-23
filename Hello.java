22c3f145679da6169d79af8f48d45ea64ff4f676
Hajar wafiq <wafiqhajaar@gmail.com>
2024-05-23 15:17:36 +0100

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        // Affiche les messages à la console
+        System.out.println("Bonjour Master devops");
+        System.out.println("Bon courage dans votre examen");
+    }
+}
